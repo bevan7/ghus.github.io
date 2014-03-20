48a9aa13b6d05d601cebb7ab6cb8ecbe2bb24e8f
bevan7 <bevanstephens@bevans-mbp.home>
2014-03-20 20:35:32 +0000

    Initial HTML and CSS for the design

---
+++ Front end template/config.js
@@ -0,0 +1,88 @@
+// we use JavaScript instead of JSON
+// the config method is defined in ghus.js
+/* global config */
+config({
+  // set name of your preferred theme name here.
+  // You can also set the URL to a custem theme CSS file
+  theme: 'none',
+  // content
+  name: 'Afri-tech Hub',
+  location: 'Nairobi, Kenya',
+  url: 'afritechhub.org',
+  description: 'Nairobi&apos;s Innovation Hub for the technology community is an open space for the technologists, investors, tech companies and hackers in the area. This space is a tech community facility with a focus on young entrepreneurs, web and mobile phone programmers, designers and researchers.',
+  coreMembers: [
+    {
+      name: 'Emmanuel Kala',
+      role: 'Developer',
+      github: 'ekala',
+      gravatar: '8fe18e746dcfdbb1ab8be5089834cf37'
+    },
+    {
+      name: 'Linda Kamau',
+      role: 'Developer',
+      github: 'kamaulynder',
+      gravatar: 'caf7b2cf0216b8dcd2140f5972cec81c'
+    },
+    {
+      name: 'Charles Kithika',
+      role: 'Developer',
+      github: 'ckithika',
+      gravatar: '3fbf25a9196fee9fa570d451ac567f3d'
+    },
+    {
+      name: 'Josephine Nekesa Were',
+      role: 'Director of Operations',
+      github: 'Nekesa',
+      gravatar: '9cbf856ed95cdcaddc22dc12a9bd6156' }
+  ],
+  communityMembers: [
+    {
+      name: 'Angela Oduor',
+      role: 'Developer',
+      github: 'aoduor',
+      gravatar: '82b7c3fab9a95ad48549827c2feaf996'
+    },
+    {
+      name: 'Erik Hersman',
+      role: 'Founder',
+      github: 'ehersman',
+      gravatar: 'b5afea347832d7bee1fcfc5c950a8893'
+    },
+    {
+      name: 'Caine Wanjau',
+      role: 'Developer',
+      github: 'cwanjau',
+      gravatar: '32eac233b9003db979848b09daf5b9cd'
+    },
+    {
+      name: 'Marie Githinji',
+      role: 'Program Manager',
+      github: 'mariegithinji',
+      gravatar: '4d1edbc93ba83e1d764c2ad48ab1d168'
+    },
+    {
+      name: 'Angela Oduor',
+      role: 'Developer',
+      github: 'aoduor',
+      gravatar: '82b7c3fab9a95ad48549827c2feaf996'
+    },
+    {
+      name: 'Erik Hersman',
+      role: 'Founder',
+      github: 'ehersman',
+      gravatar: 'b5afea347832d7bee1fcfc5c950a8893'
+    },
+    {
+      name: 'Caine Wanjau',
+      role: 'Developer',
+      github: 'cwanjau',
+      gravatar: '32eac233b9003db979848b09daf5b9cd'
+    },
+    {
+      name: 'Marie Githinji',
+      role: 'Program Manager',
+      github: 'mariegithinji',
+      gravatar: '4d1edbc93ba83e1d764c2ad48ab1d168'
+    },
+  ]
+});
